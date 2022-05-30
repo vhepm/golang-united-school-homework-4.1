1fd0f65d0d26dc91f719476c19591f60219c6cd1
Vladyslav.Hoi <vladyslav.hoi@syngenta.com>
2022-05-30 16:19:06 +0300

    Add reverse string solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,22 @@
 package reverse_string
 
+import "fmt"
+
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	n := 0
+	split := make([]rune, len(input))
+	for _, r := range input {
+		split[n] = r
+		n++
+	}
+	split = split[0:n]
+
+	for i := 0; i < n/2; i++ {
+		split[i], split[n-1-i] = split[n-1-i], split[i]
+	}
+
+	output = string(split)
+	fmt.Println(output)
+
+	return
 }
